44096f42fc990ec62a40fc225be999f5da1a80f9
MXPServer 20953 onetechguy <73059135+MXP2095onetechguy@users.noreply.github.com>
2021-09-17 15:05:26 +0700

    Add a hint.txt file for those that are wondering about the ada.adb file

--- Ada.adb
+++ Ada.adb
@@ -1,8 +1,9 @@
 
-C:\Users\Noah Lie\OneDrive\Desktop\Cthing\C#\snek>"C:\Users\Noah Lie\OneDrive\Desktop\Cthing\C#\snek\bin\Debug\net5.0\snek"  
+C:\Users\Noah Lie\OneDrive\Desktop\snek>"C:\Users\Noah Lie\OneDrive\Desktop\snek\bin\Debug\net5.0\snek"  
+O hmmm.. Reading C:\Users\Noah Lie\SnekTheGam\Storage.json
 Window handle is invalid, cannot resume.
 Full message: 
 System.IO.IOException: The handle is invalid.
-   at System.ConsolePal.set_CursorVisible(Boolean value)
-   at System.Console.set_CursorVisible(Boolean value)
-   at snek.Program.Main(String[] args) in C:\Users\Noah Lie\OneDrive\Desktop\Cthing\C#\snek\Program.cs:line 114
+   at System.ConsolePal.Clear()
+   at System.Console.Clear()
+   at SnekTheGam.Program.Main(String[] args) in C:\Users\Noah Lie\OneDrive\Desktop\snek\Program.cs:line 309
